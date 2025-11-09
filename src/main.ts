7b741fa2557d6f0a5459ce13dc1058d1e40e15d7
Claude Jordan <claude.jordan@scopevisio.com>
2025-11-09 21:35:21 +0100

    finished what do we have video

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,60 @@
-import './style.css'
-import * as THREE from 'three/webgpu'
-import { color } from 'three/tsl'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import "./style.css";
+import * as THREE from "three/webgpu";
+import { color, convertColorSpace, positionLocal, texture } from "three/tsl";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   10
-)
-camera.position.z = 1
+);
+camera.position.z = 1;
 
-const renderer = new THREE.WebGPURenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-renderer.setAnimationLoop(animate)
+const renderer = new THREE.WebGPURenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight
-  camera.updateProjectionMatrix()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-})
+window.addEventListener("resize", function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
-const controls = new OrbitControls(camera, renderer.domElement)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-const material = new THREE.NodeMaterial()
-material.fragmentNode = color('crimson')
+const material = new THREE.NodeMaterial();
+// simply set material color to crimson
+// material.fragmentNode = color("crimson");
 
-const mesh = new THREE.Mesh(new THREE.PlaneGeometry(), material)
-scene.add(mesh)
+// load  texture
+// material.fragmentNode = texture(
+//   new THREE.TextureLoader().load("https://sbcode.net/img/grid.png")
+// );
+
+// previous texture is dulled out, convert color space for more vivid culler
+// material.fragmentNode = convertColorSpace(
+//   texture(new THREE.TextureLoader().load("https://sbcode.net/img/grid.png")),
+//   THREE.SRGBColorSpace,
+//   THREE.LinearSRGBColorSpace
+// );
+
+// goes into more detail in next lesson presumably
+material.fragmentNode = positionLocal;
+
+const mesh = new THREE.Mesh(new THREE.PlaneGeometry(), material);
+scene.add(mesh);
+
+renderer.debug.getShaderAsync(scene, camera, mesh).then((e) => {
+  console.log(e.fragmentShader);
+});
 
 function animate() {
-  controls.update()
+  controls.update();
 
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 }
